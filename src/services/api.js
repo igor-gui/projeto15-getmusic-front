b7016549e22e79c005299a462e68541d80d42650
Caio Beneton <caio.beneton@gmail.com>
2022-11-25 20:54:38 -0300

    fix: api baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const baseUrl = 'http://localhost:5000'
-const api = axios.create(baseUrl)
+const api = axios.create({baseUrl})
 
-export function cadastrar(data){
-    const promise = api.post('/signup', data)
+export async function cadastrar(data){
+    const promise = await api.post('/signup', data)
     return promise
 }
 
-export function entrar(data){
-    const promise = api.post('/login', data)
+export async function entrar(data){
+    const promise = await api.post('/login', data)
     return promise
 }
