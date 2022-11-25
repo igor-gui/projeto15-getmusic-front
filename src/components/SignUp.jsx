ebe844cfab5f33161cb3d5c7b4d324910beb55a7
Igor Guimarães <igorgui.tec@gmail.com>
2022-11-25 02:56:35 -0300

    feat: API integrated to sign-up form

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,29 +3,66 @@ import Button from "./styles/Button"
 import Form from "./styles/Form"
 import { useState } from "react"
 import { changePassValue } from "../services/utilities"
+import { cadastrar } from "../services/api"
+import { useNavigate } from "react-router-dom"
 
 
 export default function SignUp() {
-    const [password, setPasswordToText] = useState('password')
-    const [confirmPassword, setConfirmPasswordPasswordToText] = useState('password')
+    const [inputType, setInputType] = useState('password')
+    const [confirmInputType, setconfirmInputType] = useState('password')
+    const eye = inputType === 'text' ? "eye-outline" : "eye-off-outline"
+    const secEye = confirmInputType === 'text' ? "eye-outline" : "eye-off-outline"
 
+    const navigate = useNavigate()
 
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const passwordsDontMatchEachOther = form.password !== form.confirmPassword
+        if (passwordsDontMatchEachOther) {
+            return alert("As senhas devem ser Iguais!!")
+        }
+        const {name, email, password} = form
+        const newForm = {
+            name,
+            email,
+            password
+        }
+        cadastrar(newForm)
+            .then((res) => {
+                alert('Cadastro realizado com sucesso!');
+                console.log(res.data)
+                navigate('/')
+            }
+            )
+            .catch((err) => console.log(err.response.data))
+    }
+
+    function handleForm(e) {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
 
     return (
         <>
 
-            <Form >
-                <Input type="text" placeholder="Nome" />
-                <Input type="text" placeholder="E-mail" />
+            <Form onSubmit={handleSubmit}>
+                <Input name="name" type="text" onChange={(e) => handleForm(e)} placeholder="Nome" />
+                <Input name="email" type="text" onChange={(e) => handleForm(e)} placeholder="E-mail" />
                 <div>
-                    <Input type={password} placeholder="Senha" />
-                    <ion-icon onClick={() => changePassValue(setPasswordToText, password)} name="eye-outline"></ion-icon>
+                    <Input name="password" type={inputType} onChange={(e) => handleForm(e)} placeholder="Senha" />
+                    <ion-icon onClick={() => changePassValue(setInputType, inputType)} name={eye}></ion-icon>
                 </div>
                 <div>
-                    <Input type={confirmPassword} placeholder="Confirmar Senha" />
-                    <ion-icon onClick={() => changePassValue(setConfirmPasswordPasswordToText, confirmPassword)} name="eye-outline"></ion-icon>
+                    <Input name="confirmPassword" type={confirmInputType} onChange={(e) => handleForm(e)} placeholder="Confirmar Senha" />
+                    <ion-icon onClick={() => changePassValue(setconfirmInputType, confirmInputType)} name={secEye}></ion-icon>
                 </div>
-                <Button value="Cadastrar" />
+                <Button type="submit" value="Cadastrar" />
             </Form>
         </>
     )
